dc84c13a3321cea8be8b8437e2189e0bc427eb66
Thais Da Silva Menelli <500449140@live.wcs.ac.uk>
2026-05-11 13:08:59 +0100

    Delete secret-place directory

--- secret-place/config.py
+++
@@ -1,8 +0,0 @@
-import os
-
-basedir = os.path.abspath(os.path.dirname(__file__))
-
-class Config:
-    SECRET_KEY = os.environ.get('SECRET_KEY') or 'your_secret_key_here'
-      
-    DATABASE = os.path.join(basedir, 'database.db')
--- secret-place/init_db.py
+++
@@ -1,9 +0,0 @@
-import sqlite3
-from config import Config
-
-conn = sqlite3.connect(Config.DATABASE)
-with open("schema.sql") as f:
-    conn.executescript(f.read())
-conn.close()
-
-print("Database initialized successfully!")
