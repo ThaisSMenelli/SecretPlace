ef46a8be5175f4868fe7048a686fc3d43140ab3b
Thais Da Silva Menelli <500449140@live.wcs.ac.uk>
2026-05-17 17:17:09 +0100

    Update script.js

--- static/js/script.js
+++ static/js/script.js
@@ -187,4 +187,19 @@ document.addEventListener("DOMContentLoaded", function () {
         }
     }
 
+    // =====================
+    // 8. Mobile Hambuerger Menu
+    // =====================
+
+    const menuToggle = document.getElementById("menuToggle");
+    const mainNav = document.getElementById("mainNav");
+
+    if (menuToggle && mainNav) {
+
+        menuToggle.addEventListener("click", () => {
+            mainNav.classList.toggle("active");
+        });
+
+    }
+
 })
